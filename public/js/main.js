53abf3aebc77f47be83d1c5fc13a8c733f0dc139
Practical Developer <practicaldeveloper1@gmail.com>
2021-07-25 20:14:04 +0200

    output messages on dom

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ const chatForm = document.getElementById('chat-form');
 const socket = io();
 
 socket.on('message', message => {
-    console.log(message);
+    outputMessage(message);
 })
 
 //Message submit
@@ -16,3 +16,19 @@ chatForm.addEventListener('submit', e => {
     //Emit message to server
     socket.emit('chatMessage', msg);
 })
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    const p = document.createElement('p');
+    p.classList.add('meta');
+    p.innerText = 'username';
+    p.innerHTML += `<span> 11.00 AM </span>`;
+    div.appendChild(p);
+    const para = document.createElement('p');
+    para.classList.add('text');
+    para.innerText = message;
+    div.appendChild(para);
+    document.querySelector('.chat-messages').appendChild(div);
+}
