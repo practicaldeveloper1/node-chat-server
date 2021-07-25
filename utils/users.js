2c7be39668dba5d25728808aba16c40fdb781178
Practical Developer <practicaldeveloper1@gmail.com>
2021-07-25 21:10:30 +0200

    add methods to remove and get users

--- utils/users.js
+++ utils/users.js
@@ -12,7 +12,7 @@ function removeUser(id) {
     const index = users.findIndex(user => user.id === id);
 
     if(index !== -1) {
-        return users.splice(index, 1);
+        return users.splice(index, 1)[0];
     }
 }
 
