391901cfde1e16ae9df6f48f75d533f052e23f48
Practical Developer <practicaldeveloper1@gmail.com>
2021-07-26 23:13:20 +0200

    make get languages request to deepl

--- api/deepl/v1/index.js
+++ api/deepl/v1/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const DEEPL_AUTH_KEY = process.env.DEEPL_AUTH_KEY;
+const DEEPL_GET_LANGUAGE_REQ = 'https://api-free.deepl.com/v2/languages'
 
-function getLanguages(res, result) {
-    return res.json({
-        status: 'success',
-        data: 'test',
-    });
+async function getLanguages(res, result) {
+    try {
+        console.log('works');
+        let config = {
+            params: { auth_key: DEEPL_AUTH_KEY },
+        }
+
+        const response = await axios.get(DEEPL_GET_LANGUAGE_REQ, config);
+        const {data} = response;
+
+        return res.json({
+            status: 'ok',
+            data
+        });
+    }
+    catch (err) {
+        console.log(err);
+        return res.json({
+           status: 'error'
+        });
+    }
 }
 
 router.get('/languages', (req, res, next) => {
