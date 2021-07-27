a5e5801cf0524660a3cccdba906a09a3d7fbaa0c
Practical Developer <practicaldeveloper1@gmail.com>
2021-07-27 23:45:09 +0200

    code cleanup

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,7 @@
 
 async function onLoadIndex() {
-    console.log('index loaded')
     const {data : {data: localesArray}} = await axios.get('http://localhost:5000/api/deepl/v1/languages');
     const selectEl = document.getElementById("locale-select");
-    console.log(localesArray)
     for (index in localesArray) {
         selectEl.options[selectEl.options.length] = new Option(localesArray[index].name, localesArray[index].language);
     }
