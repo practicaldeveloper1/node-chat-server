7595075faac29ee6dbc55992d39a524dadb94751
Practical Developer <practicaldeveloper1@gmail.com>
2021-07-28 18:04:20 +0200

    create ui that allows user to create new chatroom

--- public/js/index.js
+++ public/js/index.js
@@ -6,3 +6,20 @@ async function onLoadIndex() {
         selectEl.options[selectEl.options.length] = new Option(localesArray[index].name, localesArray[index].language);
     }
 }
+
+document.body.addEventListener('change', e => {
+    const target = e.target;
+    const chatroomExistingDiv = document.getElementById("chatroom-existing-div");
+    const chatroomNewDiv = document.getElementById("chatroom-new-div");
+    switch (target.id) {
+        case 'chatroom-new-option':
+            chatroomExistingDiv.classList.add("hidden");
+            chatroomNewDiv.classList.remove("hidden");
+            break;
+        case 'chatroom-existing-option':
+            chatroomNewDiv.classList.add("hidden");
+            chatroomExistingDiv.classList.remove("hidden");
+        default:
+            break;
+    }
+});
