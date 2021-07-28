90dfbd11c311842e12e94653cbd346f445fed866
Practical Developer <practicaldeveloper1@gmail.com>
2021-07-28 23:49:42 +0200

    create endpoint to check if chatroom exists

--- api/chat/v1/index.js
+++ api/chat/v1/index.js
@@ -1,54 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { getChatrooms } = require('../../../utils/chatrooms');
-
-async function getLanguages(res, next) {
-    try {
-        const config = {
-            params: { auth_key: DEEPL_AUTH_KEY },
-        }
-
-        const response = await axios.get(DEEPL_GET_LANGUAGE_REQ, config);
-        const { data } = response;
-
-        return res.json({
-            status: 'ok',
-            data
-        });
-    }
-    catch (err) {
-        next(err);
-    }
-}
-
-async function doTranslate(text, target_lang, res, next) {
-    try {
-        const config = {
-            params: {
-                auth_key: DEEPL_AUTH_KEY,
-                text,
-                target_lang,
-            },
-        }
-
-        const response = await axios.get(DEEPL_TRANSLATE_REQ, config);
-        const { data } = response;
-
-        return res.json({
-            status: 'ok',
-            data
-        });
-    }
-    catch (err) {
-        next(err);
-    }
-}
+const { getChatrooms, getChatroom } = require('../../../utils/chatrooms');
 
 router.get('/chatrooms', (req, res, next) => {
     const chatrooms = getChatrooms();
     return res.json({
         status: 'ok',
         data: chatrooms
+    });
+});
+
+router.get('/chatrooms/:chatroom', (req, res, next) => {
+    console.log(req.params);
+    const { chatroom: requestedChatroom } = req.params;
+    const chatroom = getChatroom(requestedChatroom);
+
+    return res.json({
+        status: 'ok',
+        data: chatroom
     });
 });
 
